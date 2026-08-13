01701bf13b5a29858443d4e0bd5cb978a5a8ea91
Vatsal <shahvatsal21@gmail.com>
2026-08-13 23:02:33 +0530

    added prisma connection

---
+++ middleware.js
@@ -0,0 +1,29 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+//createRouteMatcher will be deprecated in next release toh koi issue aaya toh issue hi theek hoga
+const isProtectedRoute = createRouteMatcher([
+    "/dashboard(.*)",
+    "/account(.*)",
+    "/transaction(.*)",
+
+])
+
+export default clerkMiddleware(async (auth,req)=>{
+    const{userId} = await auth();
+    //if user not signed in and on any of the protected routes, then we automatically redirect them to the sign in page
+    if(!userId && isProtectedRoute(req)){
+         const{redirectToSignIn} = await auth();
+
+         return redirectToSignIn();
+    }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for Clerk's auto-proxy path
+    '/__clerk/:path*',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
